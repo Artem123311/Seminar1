fda949272d2c65e49e8fe6b148ff9bb4cda75bc5
Artem <mail@example.com>
2022-08-11 05:35:19 +0300

    Показать последнюю цифру трёхзначного числа

---
+++ Project05/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите значение: ");
+string text = Console.ReadLine();
+int y = text[text.Length - 1] - '0';
+int n = y * y;
+Console.WriteLine("Последние число нашего числа {0,5}", y);
