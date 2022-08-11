97522097c9dbdc1d1b775bcbe899f1f8a849ca6e
Artem <mail@example.com>
2022-08-11 05:37:16 +0300

    Показать наибольшую цифру числа

---
+++ Project06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+ 
+namespace Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Max digit is: " + GetMaxDigit(n));
+ 
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+ 
+            for (int i = 0; i < 1000000; i++)
+                GetMaxDigit(n);
+ 
+            stopWatch.Stop();
+ 
+            Console.WriteLine("Elapsed time (ms): " + stopWatch.ElapsedMilliseconds);
+        }
+ 
+        static int GetMaxDigit(int n)
+        {
+            int max = Int32.MinValue;
+ 
+            while (n != 0)
+            {
+                int digit = n % 10;
+                if (digit > max)
+                    max = digit;
+                n /= 10;
+            }
+ 
+            return max;
+        }
+    }
+}
