06f884e05f8aede1e7bad4415460c9e7bcdede96
Artem <mail@example.com>
2022-08-11 05:19:37 +0300

    Четность числа

---
+++ Project01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+    Console.WriteLine("Even.");
+else
+    Console.WriteLine("Odd.");
