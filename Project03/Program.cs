474923baa46fbb9ca15ba2e932831655210bc8ba
Artem <mail@example.com>
2022-08-11 05:28:30 +0300

    Найти максимальное из трех чисел

---
+++ Project03/Program.cs
@@ -0,0 +1,10 @@
+﻿            double a = 50, b = 100, c = 150;
+            
+                if (a > b && a > c)
+                    Console.WriteLine("Nubmer Max = " + a); 
+                else if (b > c)
+                    Console.WriteLine("Number Max = " + b); 
+                else
+                    Console.WriteLine("Number Max = " + c); 
+            
+            Console.ReadLine();
